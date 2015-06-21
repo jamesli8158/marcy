921e6401b19254f5a4ea8cd994d0953b3a1bea29
Ubuntu <ubuntu@ip-172-31-40-69.us-west-2.compute.internal>
2015-06-21 21:26:43 +0000

    Add category

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
 var welcome = require('./routes/welcome');
+var index = require('./routes/index');
+var category = require('./routes/category');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', welcome);
 app.use('/index', index);
+app.use('/category', category);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/category.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/:category', function(req, res, next) {
+  var category = req.params.category;
+  res.render('list', {
+    category: category,
+    id: 1,
+    name: category + ' No. 1'
+  });
+});
+
+module.exports = router;
